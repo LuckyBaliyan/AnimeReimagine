b194e9ff48eb38aa92f7c05aa0afe105a5877cb5
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-08-20 15:38:26 +0530

    try

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,9 +26,12 @@ const About = () => {
             <h2 className='textsm font-["general"] mt-4  font-bold text-orange-500  uppercase md:text-[10px]'>
                 Welcome To Reimagine
             </h2>
-            <div className='mt-5 text-center text-6xl uppercase leading-0.8 md:text-[12rem] font-["zentry"]'>
+            <div className='mt-5 text-center  font-["zentry"]'>
                 {'Discover the world shared advanture'.split('').map((item,index)=>(
-                    <div id='showText' key={index} className='opacity-0 font-light inline-block'>{item+" "}</div>
+                    <div id='showText' key={index} className='opacity-0 font-light text-6xl uppercase
+                     leading-0.8 md:text-[9rem] lg:text-[12rem] inline-block'>
+                        {item+" "}
+                    </div>
                 ))}
             </div>
         </div>
