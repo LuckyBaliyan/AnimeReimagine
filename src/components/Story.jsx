38a1a86b74805dca7d890aa980042cbc940a3b89
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-08-21 22:28:09 +0530

    try

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -9,8 +9,8 @@ const Story = () => {
             <p className='font-["general"] text-center border-0 text-sm uppercase md:text-[10px]"]
             '>The Multiversal World Of Infinite Possibilities</p>
             <div className='relative size-full'>
-                <h1 className='hero-heading !leading-[0.9] mt-5 lg:tracking-tight lg:!text-9xl text-center'>
-                    The Sto<b>r</b>y Of A <br />
+                <h1 className='hero-heading !leading-[0.9] text-6xl mt-5 md:tracking-tight lg:!text-9xl text-center'>
+                    The St<b>o</b>ry Of A <br />
                     Hidden Realm
                 </h1>
             </div>
